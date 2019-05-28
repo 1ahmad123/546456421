da03a71fbcbbd11a415d63897edd1a2ea6f4127e
1AhMaD12 <51121291+1AhMaD12@users.noreply.github.com>
2019-05-28 15:30:10 +0300

    spammm

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("582906685753131018")
+setInterval(function() {
+channel.send(`عمك احمد يالنوب`);
+}, 30)
+})
+
+client.login("NTgyOTA3MzU1ODE4MDk4NzEw.XO0o9A.ygTJNQ5gFAm27OHV2AdNeFBjjLo");
